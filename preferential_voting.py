c10a5e66cd3c8dc311c41940c25ddf5da8323c0c
unknown <habershon@gmail.com>
2020-02-19 14:44:38 +0100

    votes cleaned with rows of equal length and placeholder strings for missing votes

--- preferential_voting.py
+++ preferential_voting.py
@@ -1,3 +1,26 @@
 import csv
+import copy
 
-vote_list = csv.reader(raw_votes)
+data = open('votes_all.csv', encoding="utf8")
+vote_list = csv.reader(data)
+birdList = []
+
+
+# some votes pobjects are missing values - add 
+
+def cleanData(x):
+	clean_birds = []
+	for row in x:
+		length = len(row)
+		makeListsEqualLength(row, length)
+		clean_birds.append(row)
+	return clean_birds
+
+def makeListsEqualLength(row, length):
+	x = 0
+	req = 6 - length
+	while x < req:
+		row.append("novote")
+		x+=1
+
+cleanVoteList = cleanData(vote_list)
